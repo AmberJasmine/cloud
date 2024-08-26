da30a1fea2c45df6b2279d7787b2257e450ae149
chenlu <1481094533@qq.com>
2024-08-26 20:15:38 +0800

    gateway

--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.gateway;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class GatewayApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
