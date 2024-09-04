796095d56a9d013cf57341e80dc4df932a0888fa
chenlu <1481094533@qq.com>
2024-09-04 13:56:17 +0800

    provider-user集成swagger

--- provider-user/src/main/java/com/example/provideruser/ProviderUserApplication.java
+++ provider-user/src/main/java/com/example/provideruser/ProviderUserApplication.java
@@ -3,11 +3,12 @@ package com.example.provideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class ProviderUserApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderUserApplication.class, args);
--- provider-user/src/main/java/com/example/provideruser/controller/HelloController.java
+++ provider-user/src/main/java/com/example/provideruser/controller/HelloController.java
@@ -1,18 +1,25 @@
 package com.example.provideruser.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Create by Administrator
  * Data 1:28 2021/12/16 星期四
  */
+@Api(tags = {"生产者provider-user"})
 @RestController
+@RequestMapping("user")
 public class HelloController {
 
     @GetMapping("/hello/{name}")
+    @ApiOperation(value = "生产者user测试", notes = "provider-user生产者", httpMethod = "GET")
     public String hello(@PathVariable String name) {
         return "生产者1：hello " + name;
     }
+
 }
